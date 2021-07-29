042b1cba0eb0cfa88eca902cb37b9f5781e8dea3
nakamura196 <na.kamura.1263@gmail.com>
2021-07-30 08:11:40 +0900

    fix: bug

--- resources/api/search.js
+++ resources/api/search.js
@@ -1,30 +1,41 @@
-const fs = require("fs");
+//const fs = require("fs");
+const axios = require("axios");
 
 exports.handler = function (event, context, callback) {
+  const URL = `https://nakamura196.github.io/enc_jk/list.json`;
+
   const queryStringParameters = event.queryStringParameters;
   const q = queryStringParameters.q;
 
   const limit = 2000;
 
-  const jsonObject = JSON.parse(fs.readFileSync("list.json", "utf8"));
+  return axios
+    .get(URL)
+    .then(function (response) {
+      const jsonObject = response.data;
 
-  const items = [];
+      const items = [];
 
-  for (let label in jsonObject) {
-    if (!q || label.includes(q)) {
-      items.push({
-        label,
-        id: jsonObject[key].split("=")[1],
+      for (let label in jsonObject) {
+        if (!q || label.includes(q)) {
+          items.push({
+            label,
+            id: jsonObject[label].split("=")[1],
+          });
+        }
+
+        if (items.length >= limit) {
+          break;
+        }
+      }
+
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(items),
       });
-    }
-
-    if (items.length >= limit) {
-      break;
-    }
-  }
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(items),
-  });
+    })
+    .catch((e) => ({
+      statusCode: 400,
+      body: e,
+    }));
 };
