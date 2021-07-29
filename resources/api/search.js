216453a69c86d4b5eb607f36ced8cc0fdaa023d4
nakamura196 <na.kamura.1263@gmail.com>
2021-07-30 07:50:12 +0900

    feat: add search functions

--- resources/api/search.js
+++ resources/api/search.js
@@ -1,19 +1,30 @@
 const fs = require("fs");
 
 exports.handler = function (event, context, callback) {
-  const body = { aaa: "bbb" };
-
   const queryStringParameters = event.queryStringParameters;
   const q = queryStringParameters.q;
 
-  if (q) {
-    console.log({ q });
-  }
+  const limit = 2000;
 
   const jsonObject = JSON.parse(fs.readFileSync("list.json", "utf8"));
 
+  const items = [];
+
+  for (let label in jsonObject) {
+    if (!q || label.includes(q)) {
+      items.push({
+        label,
+        id: jsonObject[key].split("=")[1],
+      });
+    }
+
+    if (items.length >= limit) {
+      break;
+    }
+  }
+
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(jsonObject),
+    body: JSON.stringify(items),
   });
 };
